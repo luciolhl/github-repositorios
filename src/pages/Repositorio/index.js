53fdd405dab92dc81ac4b2abbd09f6ee3bd1dd86
Lucio Henrique Lourenço <lucio.henrique@take.net>
2022-06-27 10:56:11 -0300

    Add details repos

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Container } from './styles';
+import { Container, Owner, Loading, BackButton, IssuesList } from './styles';
+import { FaArrowLeft } from 'react-icons/fa';
 import api from '../../services/api';
 
 export default function Repositorio(props){
@@ -33,9 +34,45 @@ export default function Repositorio(props){
         load();
     }, [repositorio]);
 
+    if(loading){
+        return(
+            <Loading>
+                <h1>Carregando...</h1>
+            </Loading>
+        )
+    }
     return(
         <Container>
+            <BackButton to="/">
+                <FaArrowLeft color="#000" size={30} />
+            </BackButton>
 
+            <Owner>
+                <img src={repositorios.owner.avatar_url} alt={repositorios.owner.login}/>
+                <h1>{repositorios.name}</h1>
+                <p>{repositorios.description}</p>
+            </Owner>
+
+            <IssuesList>
+                {issues.map(issue => (
+                    <li key={String(issue.id)}>
+                        <img src={issue.user.avatar_url} alt={issue.user.login} />
+                        <div>
+                            <strong>
+                                <a href={issue.html_url}>{issue.title}</a>
+
+                                {issue.labels.map(label => (
+                                    <span key={String(label.id)}>
+                                        {label.name}
+                                    </span>
+                                ))}
+                            </strong>
+
+                            <p>{issue.user.login}</p>
+                        </div>
+                    </li>
+                ))}
+            </IssuesList>
         </Container>
     )
 }
